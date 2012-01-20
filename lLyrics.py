5fe88eb12a1268df65a61bc11827dfd182eb8feb
dmo60 <timoloewe91@googlemail.com>
2012-01-21 00:57:59 +0100

    added textview and recognition of current playing song

--- lLyrics.py
+++ lLyrics.py
@@ -15,7 +15,11 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         """
         print "activate plugin lLyrics"
         self.shell = self.object
-        self.init_sidebar()
+        player = self.shell.props.shell_player
+        self.player_cb_ids = (
+                              player.connect ('playing-song-changed', self.init_sidebar)
+        )
+#        self.init_sidebar()
 
     def do_deactivate(self):
         """
@@ -25,7 +29,7 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         self.shell.remove_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR)
         
         
-    def init_sidebar(self):
+    def init_sidebar(self, player, entry):
         self.vbox = Gtk.VBox()
         frame = Gtk.Frame()
         self.label = Gtk.Label(_("Lyrics"))
@@ -34,9 +38,30 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         frame.set_label_widget(self.label)
         self.label.set_use_markup(True)
         self.label.set_padding(0,4)
+        
+        if entry is not None:
+            artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
+            title = entry.get_string(RB.RhythmDBPropType.TITLE)
+        else:
+            artist = "none"
+            title = "none"        
+        
+        textview = Gtk.TextView()
+        textbuffer = Gtk.TextBuffer()
+        textbuffer.set_text(artist + " - " + title)
+        textview.set_buffer(textbuffer)
+        textview.set_editable(False)
+        textview.set_cursor_visible(False)
+        textview.set_left_margin(10)
+        textview.set_right_margin(10)
+        textview.set_pixels_above_lines(10)
+        textview.set_pixels_below_lines(10)
+        textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        
 
         #---- pack everything into side pane ----#
         self.vbox.pack_start  (frame, False, True, 0)
+        self.vbox.pack_start (textview, True, True, 10)
 
         self.vbox.show_all()
         self.vbox.set_size_request(200, -1)
