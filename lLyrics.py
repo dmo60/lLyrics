54de4914c51b31bd0a1913707ebbdb763e2b3acd
dmo60 <timoloewe91@googlemail.com>
2012-01-21 02:00:49 +0100

    corrected activation and deactivation

--- lLyrics.py
+++ lLyrics.py
@@ -12,58 +12,78 @@ class lLyrics(GObject.GObject, Peas.Activatable):
     def do_activate(self):
         """
         activate plugin
-        """
-        print "activate plugin lLyrics"
+        """        
         self.shell = self.object
-        player = self.shell.props.shell_player
-        self.player_cb_ids = (
-                              player.connect ('playing-song-changed', self.init_sidebar)
-        )
-#        self.init_sidebar()
+        self.init_sidebar()
+
+        self.player = self.shell.props.shell_player
+        if self.player.props.playing:
+                self.search_lyrics(self.player, self.player.get_playing_entry())
+        self.psc_id = self.player.connect ('playing-song-changed', self.search_lyrics)
+        
+        print "activated plugin lLyrics"
 
     def do_deactivate(self):
         """
         deactivate plugin
-        """
-        print "deactivate plugin lLyrics"
-        self.shell.remove_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR)
+        """        
+        if self.visible:
+            self.shell.remove_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR)
+        self.vbox = None
+        self.textview = None
+        self.textbuffer = None
+        self.player.disconnect(self.psc_id)
+        del self.psc_id
+        self.shell = None
+        self.player_cb_ids = None
+        self.visible = None
+        self.player = None
         
-        
-    def init_sidebar(self, player, entry):
+        print "deactivated plugin lLyrics"
+       
+    def init_sidebar(self):
         self.vbox = Gtk.VBox()
         frame = Gtk.Frame()
-        self.label = Gtk.Label(_("Lyrics"))
+        label = Gtk.Label(_("Lyrics"))
         frame.set_shadow_type(Gtk.ShadowType.IN)
         frame.set_label_align(0.0,0.0)
-        frame.set_label_widget(self.label)
-        self.label.set_use_markup(True)
-        self.label.set_padding(0,4)
+        frame.set_label_widget(label)
+        label.set_use_markup(True)
+        label.set_padding(0,4)
         
+        # create a TextView for displaying lyrics
+        self.textview = Gtk.TextView()
+        self.textview.set_editable(False)
+        self.textview.set_cursor_visible(False)
+        self.textview.set_left_margin(10)
+        self.textview.set_right_margin(10)
+        self.textview.set_pixels_above_lines(10)
+        self.textview.set_pixels_below_lines(10)
+        self.textview.set_wrap_mode(Gtk.WrapMode.WORD)
+        
+        # initialize a TextBuffer to store lyrics in
+        self.textbuffer = Gtk.TextBuffer()
+
+        #---- pack everything into side pane ----#
+        self.vbox.pack_start  (frame, False, True, 0)
+        self.vbox.pack_start (self.textview, True, True, 10)
+
+        self.vbox.show_all()
+        self.vbox.set_size_request(200, -1)
+        self.visible = False
+        
+    def search_lyrics(self, player, entry):
+        if not self.visible:
+            self.shell.add_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR, True, True)
+            self.visible = True
+
         if entry is not None:
             artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
             title = entry.get_string(RB.RhythmDBPropType.TITLE)
         else:
             artist = "none"
-            title = "none"        
-        
-        textview = Gtk.TextView()
-        textbuffer = Gtk.TextBuffer()
-        textbuffer.set_text(artist + " - " + title)
-        textview.set_buffer(textbuffer)
-        textview.set_editable(False)
-        textview.set_cursor_visible(False)
-        textview.set_left_margin(10)
-        textview.set_right_margin(10)
-        textview.set_pixels_above_lines(10)
-        textview.set_pixels_below_lines(10)
-        textview.set_wrap_mode(Gtk.WrapMode.WORD)
-        
-
-        #---- pack everything into side pane ----#
-        self.vbox.pack_start  (frame, False, True, 0)
-        self.vbox.pack_start (textview, True, True, 10)
-
-        self.vbox.show_all()
-        self.vbox.set_size_request(200, -1)
-        self.shell.add_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR, True, True)
+            title = "none"     
+            
+        self.textbuffer.set_text(artist + " - " + title)
+        self.textview.set_buffer(self.textbuffer)
     
