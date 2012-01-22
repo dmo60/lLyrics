1b9bc491e3d07bb0d28f74da4e28f46accaee7d5
dmo60 <timoloewe91@googlemail.com>
2012-01-22 17:41:13 +0100

    now uses SearchLyricDirect API request since Chartlyrics-API seems to have problems with multiple consecutive requests

--- chartlyricsParser.py
+++ chartlyricsParser.py
@@ -1,4 +1,8 @@
 # Parser for Chartlyrics.com API
+#
+# Chartlyrics API seems to have problems with multiple consecutive requests
+# (it apparently requires a 20-30sec interval between two API-calls), 
+# so just use SearchLyricDirect since it only needs one API request. 
 
 import urllib2, time, string
 from HTMLParser import HTMLParser
@@ -10,10 +14,8 @@ class chartlyricsParser(HTMLParser):
         self.artist = artist
         self.title = title
         self.tag = None
-        self.lyricId = None
-        self.checksum = None
-        self.found = False
-        self.lyric = "no lyrics found"
+        self.correct = True
+        self.lyrics = ""
     
     # define handler for parsing             
     def handle_starttag(self, tag, attrs):
@@ -21,67 +23,35 @@ class chartlyricsParser(HTMLParser):
     
     # definde handler for parsing    
     def handle_endtag(self, tag):
-        if self.found and tag == "searchlyricresult":
-            self.found = False
         self.tag = None
     
     # definde handler for parsing               
     def handle_data(self, data):
-        if self.checksum is None and self.tag == "lyricchecksum":
-            self.checksum = data
-            self.found = True
-            return      
-        
-        if self.lyricId is None and self.tag == "lyricid":
-            self.lyricId = data
-            return
-            
-        if self.found and self.tag == "artist":
-            if data.lower() != self.artist:
-                self.lyricId = None
-                self.checksum = None
-                self.found = False
-            return
-                
-        if self.found and self.tag == "song":
+        if self.tag == "lyricsong":
             if data.lower() != self.title:
-                self.lyricId = None
-                self.checksum = None
-                self.found = False
+                self.correct = False
             return
-                
-        if self.tag == "lyric":
-            self.lyric = data
+        if self.correct and self.tag == "lyricsartist":
+            if data.lower() != self.artist:
+                self.correct = False
+            return
+        if self.correct and self.tag == "lyric":
+            self.lyrics = data
             print "found lyrics"
         
     def parse(self):
         # API searchLyric request
-        url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=" + urllib2.quote(self.artist) + "&song=" + urllib2.quote(self.title)
+        url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyricDirect?artist=" + urllib2.quote(self.artist) + "&song=" + urllib2.quote(self.title)
         print "call chartlyrics API: " + url
         req = urllib2.Request(url)
-        while True:
-            try:
-                resp = urllib2.urlopen(req).read()
-                break
-            except:
-                time.sleep(2)
-        self.feed(resp)
-        
-        # API getLyric request
-        if self.lyricId is not None and self.checksum is not None:
-            url = "http://api.chartlyrics.com/apiv1.asmx/GetLyric?lyricId=" + self.lyricId + "&lyricCheckSum=" + self.checksum
-            print "call chartlyrics API: " + url
-            req = urllib2.Request(url)
-            while True:
-                try:
-                    resp = urllib2.urlopen(req).read()
-                    break
-                except: 
-                    time.sleep(2) 
+        try:
+            resp = urllib2.urlopen(req).read()
             self.feed(resp)
-        else:
-            # searchLyric was not successful
+        except:
+            print "could not connect to chartlyric.com API"
+        
+        if self.lyrics == "":
             print "no lyrics found"
         
-        return self.lyric
+        return self.lyrics
         
