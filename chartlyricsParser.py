1a6acb9a6bb0ef3bf15a642200682a48cf10bd1e
dmo60 <timoloewe91@googlemail.com>
2012-01-22 14:30:50 +0100

    check matching of artist and title of search results

--- chartlyricsParser.py
+++ chartlyricsParser.py
@@ -5,58 +5,78 @@ class chartlyricsParser(HTMLParser):
     
     def __init__(self, artist, title):
         HTMLParser.__init__(self)
-        self.artist = artist.replace(" ", "%20")
-        self.title = title.replace(" ", "%20")
+        self.artist = artist
+        self.title = title
         self.tag = None
         self.lyricId = None
         self.checksum = None
-        self.lyric = "not found"
-        self.found = False
-    
+        self.set = False
+        self.lyric = "no lyrics found"
+        
+        print "search lyrics for " + artist + " - " + title
+        
     def handle_starttag(self, tag, attrs):
         self.tag = tag
-        print "starttag " + tag
         
     def handle_endtag(self, tag):
+        if self.set and tag == "searchlyricresult":
+            self.set = False
         self.tag = None
-#        print "endtag " + tag
                     
     def handle_data(self, data):
 #        print "data " + data
-        if self.found:
-            return
-        if self.lyricId is None and self.tag == "lyricid":
-            self.lyricId = data
-            print "set lyricId to " + self.lyricId
         if self.checksum is None and self.tag == "lyricchecksum":
             self.checksum = data
-            print "set checksum to " + self.checksum
+            self.set = True
+            return      
+        
+        if self.lyricId is None and self.tag == "lyricid":
+            self.lyricId = data
+            return
+            
+        if self.set and self.tag == "artist":
+            if data != self.artist:
+                self.lyricId = None
+                self.checksum = None
+                self.set = False
+            return
+                
+        if self.set and self.tag == "song":
+            if data != self.title:
+                self.lyricId = None
+                self.checksum = None
+                self.set = False
+            return
+                
         if self.tag == "lyric":
             self.lyric = data
             print "found lyrics"
         
     def parse(self):
-        url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=" + self.artist + "&song=" + self.title
-        print url
-        req = urllib2.Request(url)
-        try:
-            resp = urllib2.urlopen(req).read()
-        except:
-            None
-        self.feed(resp)
-        
-        url = "http://api.chartlyrics.com/apiv1.asmx/GetLyric?lyricId=" + self.lyricId + "&lyricCheckSum=" + self.checksum
-        print url
+        url = "http://api.chartlyrics.com/apiv1.asmx/SearchLyric?artist=" + self.artist.replace(" ", "%20") + "&song=" + self.title.replace(" ", "%20")
+        print "call chartlyrics API: " + url
         req = urllib2.Request(url)
         while True:
             try:
                 resp = urllib2.urlopen(req).read()
                 break
-                print resp
-            except: 
+            except:
                 time.sleep(2)
-            
         self.feed(resp)
+        
+        if self.lyricId is not None and self.checksum is not None:
+            url = "http://api.chartlyrics.com/apiv1.asmx/GetLyric?lyricId=" + self.lyricId + "&lyricCheckSum=" + self.checksum
+            print "call chartlyrics API: " + url
+            req = urllib2.Request(url)
+            while True:
+                try:
+                    resp = urllib2.urlopen(req).read()
+                    break
+                except: 
+                    time.sleep(2) 
+            self.feed(resp)
+        else:
+            print "no lyrics found"
         
         return self.lyric
         
