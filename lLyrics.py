5b93783f52f31d52183afda1752169f5f3dfd1dd
dmo60 <timoloewe91@googlemail.com>
2012-01-22 14:29:27 +0100

    searchLyrics in own Thread added toggel_visibility action to menu

--- lLyrics.py
+++ lLyrics.py
@@ -1,7 +1,19 @@
-from gi.repository import GObject, Peas
+from gi.repository import GObject, Peas, Gdk
 from gi.repository import RB
 from gi.repository import Gtk
+from threading import Thread
 import chartlyricsParser
+
+llyrics_ui = """
+<ui>
+    <menubar name="MenuBar">
+        <menu name="ViewMenu" action="View">
+            <menuitem name="Lyrics" action="ToggleLyricSideBar" />
+        </menu>
+    </menubar>
+</ui>
+"""
+
 
 class lLyrics(GObject.GObject, Peas.Activatable):
     __gtype_name = 'lLyrics'
@@ -9,6 +21,8 @@ class lLyrics(GObject.GObject, Peas.Activatable):
     
     def __init__(self):
         GObject.GObject.__init__(self)
+        GObject.threads_init()
+        Gdk.threads_init()
 
     def do_activate(self):
         """
@@ -24,6 +38,17 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         # search lyrics if song changes 
         self.psc_id = self.player.connect ('playing-song-changed', self.search_lyrics)
         
+        # Add button to toggle visibility of pane
+        self.action = ('ToggleLyricSideBar','gtk-info', _("Lyrics Sidebar"),
+                        None, _("Change the visibility of the lyrics sidebar"),
+                        self.toggle_visibility, True)
+        self.action_group = Gtk.ActionGroup(name='lLyricsPluginActions')
+        self.action_group.add_toggle_actions([self.action])
+        uim = self.shell.props.ui_manager
+        uim.insert_action_group (self.action_group, 0)
+        self.ui_id = uim.add_ui_from_string(llyrics_ui)
+        uim.ensure_update()
+        
         print "activated plugin lLyrics"
 
     def do_deactivate(self):
@@ -37,11 +62,17 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         self.textbuffer = None
         self.player.disconnect(self.psc_id)
         del self.psc_id
-        self.shell = None
         self.player_cb_ids = None
         self.visible = None
         self.player = None
+        uim = self.shell.props.ui_manager
+        uim.remove_ui (self.ui_id)
+        uim.remove_action_group (self.action_group)
+        self.action = None
+        self.action_group = None
         
+        self.shell = None
+
         print "deactivated plugin lLyrics"
        
     def init_sidebar(self):
@@ -70,6 +101,8 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         
         # initialize a TextBuffer to store lyrics in
         self.textbuffer = Gtk.TextBuffer()
+        self.textview.set_buffer(self.textbuffer)
+
 
         # pack everything into side pane
         self.vbox.pack_start  (frame, False, True, 0)
@@ -77,15 +110,23 @@ class lLyrics(GObject.GObject, Peas.Activatable):
 
         self.vbox.show_all()
         self.vbox.set_size_request(200, -1)
-        # do not add widget yet, this will be done when user starts playback
-        self.visible = False
-        
-    def search_lyrics(self, player, entry):
-        # show sidebar at first playback or when reactivating plugin during playback
+        self.shell.add_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR, True, True)
+        self.visible = True 
+    
+    def toggle_visibility (self, action):
         if not self.visible:
             self.shell.add_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR, True, True)
             self.visible = True
-
+        else:
+            self.shell.remove_widget (self.vbox, RB.ShellUILocation.RIGHT_SIDEBAR)
+            self.visible = False
+        
+    def search_lyrics(self, player, entry):
+        self.textbuffer.set_text("searching lyrics...")
+        newthread = Thread(target=self._search_lyrics_thread, args=(player, entry))
+        newthread.start()
+    
+    def _search_lyrics_thread(self, player, entry):
         if entry is not None:
             artist = entry.get_string(RB.RhythmDBPropType.ARTIST)
             title = entry.get_string(RB.RhythmDBPropType.TITLE)
@@ -95,6 +136,8 @@ class lLyrics(GObject.GObject, Peas.Activatable):
         
         parser = chartlyricsParser.chartlyricsParser(artist, title)
         lyrics = parser.parse()
+        Gdk.threads_enter()
         self.textbuffer.set_text(artist + " - " + title + "\n" + lyrics)
-        self.textview.set_buffer(self.textbuffer)
+        Gdk.threads_leave()
+
     
