edca5c487bdc98c09eb2f82e732d297b0b50d6de
dmo60 <timoloewe91@googlemail.com>
2012-01-28 20:40:19 +0100

    check type of keys

--- lLyrics/Config.py
+++ lLyrics/Config.py
@@ -13,6 +13,7 @@ class Config():
         self.gconf_client.add_dir(GCONF_DIR, gconf.CLIENT_PRELOAD_RECURSIVE)
         self.init_sources_key()
         self.init_cache_key()
+        
     
     def init_sources_key(self):        
         # create and set key if it doesn't exist
@@ -20,6 +21,13 @@ class Config():
             self.gconf_client.set_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING, lLyrics.LYRIC_SOURCES)
             print "set gconf lyrics_sources default"
             return
+        
+        # check correct type
+        if self.gconf_client.get_without_default(GCONF_DIR + "/lyrics_sources").type != gconf.VALUE_LIST:
+            self.gconf_client.set_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING, lLyrics.LYRIC_SOURCES)
+            print "set gconf lyrics_sources default"
+            return  
+              
         # remove invalid entries
         changed = False
         sources_list = self.gconf_client.get_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING)
@@ -28,23 +36,35 @@ class Config():
                 sources_list.remove(entry)
                 changed = True
                 print "invalid gconf sources entry: " + entry
+                
         # if list is empty, set default
         if len(sources_list) == 0:
             self.gconf_client.set_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING, lLyrics.LYRIC_SOURCES)
             print "set gconf lyrics_sources default"
             return
+        
         # update key, if changed
         if changed:
             self.gconf_client.set_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING, sources_list)   
+    
     
     def init_cache_key(self):
         # create and set key if it doesn't exist
         if self.gconf_client.get_without_default(GCONF_DIR + "/cache_lyrics") is None:
             self.gconf_client.set_bool(GCONF_DIR + "/cache_lyrics", True)
             print "set gconf cache_lyrics default"
+            return
+        
+        # check correct type
+        if self.gconf_client.get_without_default(GCONF_DIR + "/cache_lyrics").type != gconf.VALUE_BOOL:
+            self.gconf_client.set_bool(GCONF_DIR + "/cache_lyrics", True)
+            print "set gconf cache_lyrics default"
+            return
+        
     
     def get_lyrics_sources(self):
         return self.gconf_client.get_list(GCONF_DIR + "/lyrics_sources", gconf.VALUE_STRING)
+    
     
     def get_cache_lyrics(self):
         return self.gconf_client.get_bool(GCONF_DIR + "/cache_lyrics")
