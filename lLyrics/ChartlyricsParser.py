786e35c02d16a9f27be0267481f1b5a888caaabc
dmo60 <timoloewe91@googlemail.com>
2012-01-30 13:08:52 +0100

    removed copyright statements

--- lLyrics/ChartlyricsParser.py
+++ lLyrics/ChartlyricsParser.py
@@ -4,8 +4,6 @@
 # (it apparently requires a 20-30sec interval between two API-calls), 
 # so just use SearchLyricDirect since it only needs one API request.
 
-# Copyright (C) 2012 Timo Loewe
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
--- lLyrics/TerraParser.py
+++ lLyrics/TerraParser.py
@@ -1,7 +1,5 @@
 # Parser for letras.terra.com.br
 
-# Copyright (C) 2012 Timo Loewe
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 2 of the License, or
