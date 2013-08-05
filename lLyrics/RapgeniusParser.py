ae574dab8fd4b968a5a6a69994ee5ac094883823
Bersam Karbasion <bersam.k@gmail.com>
2013-08-05 04:35:51 +0430

    utf-8 support

--- lLyrics/RapgeniusParser.py
+++ lLyrics/RapgeniusParser.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Parser for Rapgenius.com
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
