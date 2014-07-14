c7ef82b86f42fa0ef6373d74c359e717cbec5223
MovingtoMars <legoliam11@gmail.com>
2014-07-14 13:37:44 +1200

    Fixed instrumentals bug
    Allowed the user to mark a song as an instrumental while the plugin is still searching for lyrics

--- lLyrics/lLyrics.py
+++ lLyrics/lLyrics.py
@@ -443,8 +443,8 @@ class lLyrics(GObject.Object, Peas.Activatable):
     def set_menu_sensitive(self, sensitive):
         index = 0
         for item in self.menu:
-            # 'Preferences' option should always be sensitive
-            if index == len(self.menu)-1:
+            # 'Preferences' and 'Mark as instrumental' options should always be sensitive
+            if item.get_label()=='Preferences' or item.get_label()=="Mark as instrumental":
                 continue
             item.set_sensitive(sensitive)
             index += 1
@@ -609,6 +609,7 @@ class lLyrics(GObject.Object, Peas.Activatable):
         self.write_lyrics_to_cache(self.path, lyrics)
         self.current_source = None
         Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.show_lyrics, lyrics)
+        self.set_menu_sensitive(True)
         
         
         
@@ -801,6 +802,10 @@ class lLyrics(GObject.Object, Peas.Activatable):
         if artist != self.clean_artist or title != self.clean_title:
             print("song changed")
             return          
+        # check is source is removed (probably because it's been set as instrumental)
+        if self.current_source == None:
+            print("current source removed (likely set as instrumental)")
+            return
         
         Gdk.threads_add_idle(GLib.PRIORITY_DEFAULT_IDLE, self.set_menu_sensitive, True)
         
@@ -832,6 +837,10 @@ class lLyrics(GObject.Object, Peas.Activatable):
                 if artist != self.clean_artist or title != self.clean_title:
                     print("song changed")
                     return
+                # check is source is removed (probably because it's been set as instrumental)
+                if self.current_source == None:
+                    print("current source removed (likely set as instrumental)")
+                    return
                 i += 1
         
         # We can't display new lyrics while user is editing! 
@@ -840,6 +849,10 @@ class lLyrics(GObject.Object, Peas.Activatable):
         # check if playing song changed
         if artist != self.clean_artist or title != self.clean_title:
             print("song changed")
+            return
+        # check is source is removed (probably because it's been set as instrumental)
+        if self.current_source == None:
+            print("current source removed (likely set as instrumental)")
             return
             
         if lyrics == "": 
