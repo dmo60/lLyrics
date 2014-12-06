415a934da69a591b5225c1f49b09e93d8d75e515
Seff Qin <seff.qin@gmail.com>
2014-12-06 16:36:36 +0800

    Add parser for Baidu.com

---
+++ lLyrics/BaiduParser.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Filename: engine_baidu.py
+
+import engine
+# import re
+import urllib
+#import chardet
+from bs4 import BeautifulSoup
+
+
+UA = "Mozilla/5.0 (iPad; CPU OS 5_1_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B206 Safari/7534.48.3"
+
+
+class Parser(engine.engine):
+
+    def __init__(self, artist, title):
+        engine.engine.__init__(self, proxy=None, locale="utf-8")
+        self.artist = artist
+        self.title = title
+        self.found = True
+        self.lyric_url = None
+        self.lyrics = ""
+        self.proxy = None
+
+    def request(self):
+        url1 = 'http://music.baidu.com/search/lrc?key='
+        url2_pre = '%s %s' % (self.title, self.artist)
+        url2 = urllib.parse.quote(url2_pre)
+        url = url1 + url2
+
+        try:
+            #request = urllib2.Request(url)
+            #opener = urllib2.build_opener()
+            #request.add_header('User-Agent', UA)
+            #content = opener.open(request).read()
+            content = urllib.request.urlopen(url).read()
+        except IOError:
+            return (None, True)
+        else:
+            lrc_list = []
+            soup = BeautifulSoup(content)
+            # num = soup.find(attrs={'class': re.compile("number")}).get_text()
+            try:
+                li = soup.find(name='div', attrs={'class': r"lrc-list"}).find_all('li')
+            except:
+                return (None, True)
+            for i in li:
+                try:
+                    ti = i.find(attrs={'class': 'song-title'}).get_text().split()[1]
+                except:
+                    ti = ''
+                try:
+                    ar = i.find(attrs={'class': 'artist-title'}).get_text().split()[1]
+                except:
+                    ar = ''
+                try:
+                    al = i.find(attrs={'class': 'album-title'}).get_text().split()[1]
+                except:
+                    al = ''
+                try:
+                    lrc_url = i.find(attrs={'class': 'down-lrc-btn'}).get('class')[2].split("'")[3]
+                    lrc_url = "http://music.baidu.com" + lrc_url
+                except:
+                    lrc_url = ''
+                lrc_list.append([ti, ar, al, lrc_url])
+        return (lrc_list, False)
+
+    def parse(self):
+        (lrcList, flag) = self.request()
+        if flag == True or lrcList is None:
+            return ""
+        else:
+            lrcUrl = lrcList[0][3]
+            print(lrcUrl)
+            lyrics, check = self.downIt(lrcUrl)
+#            detect_dict = chardet.detect(lyrics)
+#            try:
+#                confidence, encoding = detect_dict['confidence'], detect_dict['encoding']
+#            except:
+#                encoding = 'gb18030'
+#            print(encoding)
+#            lyrics = lyrics.decode(encoding, 'ignore')
+#            lyrics = lyrics.encode("utf-8", "replace")
+            return lyrics
---
+++ lLyrics/engine.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+import urllib
+import re
+
+
+def remove_html_tag(data):
+    p = re.compile(r'<.*?>')
+    return p.sub('', data)
+
+
+def order_results(results, artist, title):
+    comp = [artist, title]
+    results = map(lambda x: x[1], reversed(sorted(((cmp_result(comp, i), i) for i in results))))
+    return results
+
+
+def cmp_result(result, comp):
+    value = similarity(result[0], comp[0])  # artist
+    value += similarity(result[1], comp[1])  # title
+    value = value / 2
+    return value
+
+
+def similarity(string1, string2):
+    if string1.lower() == string2.lower():
+        return 1
+    string1 = tokenize(string1.lower())
+    string2 = tokenize(string2.lower())
+    count = len(tuple((i for i in string1 if i in string2)))
+    return float(count) / max((len(string2), 1))
+
+
+def tokenize(string):
+    string = tuple((i.lower() for i in re.findall("\w+", string) if i.lower() not in ('a', 'an', 'the')))
+    return string
+
+
+def valid_lrc(lrc):
+    partial = "".join((i for i in lrc if(ord(i) < 128 and ord(i) != 0)))
+    return bool(re.search('\[\d+:\d+.*?\]', partial))
+
+
+class engine:
+
+    def __init__(self, proxy=None, locale="utf-8", check=True):
+        if locale is None:
+            locale = "utf-8"
+        self.locale = locale
+        self.proxy = proxy
+        self.net_encoder = None
+        self.need_check = check
+
+    def request(self, artist, title):
+        raise NotImplementedError('request must be implemented in subclass')
+
+    def downIt(self, url):
+        try:
+            ff = urllib.request.urlopen(url)
+#            original_lrc = re.sub('<br/>', '\n', ff.read())
+            original_lrc = re.sub('<br/>', '\n', ff.read().decode('utf-8'))
+            ff.close()
+        except IOError:
+            return (None, True)
+        else:
+            if(self.need_check):
+                if(self.net_encoder is None or self.net_encoder.startswith("utf-16") or self.net_encoder.startswith("utf-32")):
+                    if not self.valid_lrc(original_lrc):
+                        original_lrc = None
+                    elif(not re.search('\[\d+:\d+.*?\]', original_lrc)):
+                        original_lrc = None
+            return (original_lrc, False)
+
+    def order_results(self, results, artist, title):
+        return order_results(results, artist, title)
+
+    def valid_lrc(self, lrc):
+        return valid_lrc(lrc)
