922f2d1c55e5cc17aa8378ee2aa4d2b40ecc81e6
Pranjal Jain <24854511+yellowjacketcoder@users.noreply.github.com>
2022-03-24 11:59:48 +0530

    update changes

--- lLyrics/lLyrics.py
+++ lLyrics/lLyrics.py
@@ -94,8 +94,8 @@ LYRICS_TITLE_STRIP = ["\(live[^\)]*\)", "\(acoustic[^\)]*\)", "\([^\)]*mix\)", "
 LYRICS_TITLE_REPLACE = [("/", "-"), (" & ", " and ")]
 LYRICS_ARTIST_REPLACE = [("/", "-"), (" & ", " and ")]
 
-LYRICS_SOURCES = ["Musixmatch", "Lyricwiki.org", "Letras.terra.com.br", "Metrolyrics.com", "AZLyrics.com", "Lyricsmania.com",
-                  "Vagalume.com.br", "Genius.com", "Darklyrics.com", "Chartlyrics.com"]
+LYRICS_SOURCES = ["Lyricwiki.org", "Letras.terra.com.br", "Metrolyrics.com", "AZLyrics.com", "Lyricsmania.com",
+                  "Vagalume.com.br", "Genius.com", "Darklyrics.com", "Chartlyrics.com", "Musixmatch"]
 
 
 class lLyrics(GObject.Object, Peas.Activatable):
@@ -114,11 +114,11 @@ class lLyrics(GObject.Object, Peas.Activatable):
         self.appshell = ApplicationShell(self.shell)
 
         # Create dictionary which assigns sources to their corresponding modules
-        self.dict = dict({"Musixmatch": MusixmatchParser, "Lyricwiki.org": LyricwikiParser,
-                          "Letras.terra.com.br": LetrasTerraParser, "Metrolyrics.com": MetrolyricsParser,
-                          "AZLyrics.com": AZLyricsParser, "Lyricsmania.com": LyricsmaniaParser,
-                          "Chartlyrics.com": ChartlyricsParser, "Darklyrics.com": DarklyricsParser,
-                          "Genius.com": GeniusParser, "Vagalume.com.br": VagalumeParser})
+        self.dict = dict({"Lyricwiki.org": LyricwikiParser, "Letras.terra.com.br": LetrasTerraParser,
+                          "Metrolyrics.com": MetrolyricsParser, "AZLyrics.com": AZLyricsParser,
+                          "Lyricsmania.com": LyricsmaniaParser, "Chartlyrics.com": ChartlyricsParser,
+                          "Darklyrics.com": DarklyricsParser, "Genius.com": GeniusParser,
+                          "Vagalume.com.br": VagalumeParser, "Musixmatch": MusixmatchParser})
         self.add_builtin_lyrics_sources()
 
         # Get the user preferences
